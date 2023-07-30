b69e95e3d151b9c778dbe7d7e637b2deb11cbbd5
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-07-30 10:02:23 -0700

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,3 +14,17 @@ def status():
     Returns a JSON status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieve the number of each object type"""
+    stats_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats_dict)
