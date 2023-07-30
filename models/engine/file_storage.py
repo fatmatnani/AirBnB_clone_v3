9e6ad29833252d28ef06b2a4bc5c94301b0e38e2
fatma tnani <fatmatnani08@gmail.com>
2023-07-30 12:51:08 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+   def get(self, cls, id):
+       """Retrieve one object"""
+       if cls and id:
+           key = cls.__name__ + '.' + id
+           return self.__objects.get(key)
+       return None
+   
+   def count(self, cls=None):
+       """Count the number of objects in storage"""
+       if cls:
+           return len(self.all(cls))
+       else:
+           return len(self.__objects)
+        
