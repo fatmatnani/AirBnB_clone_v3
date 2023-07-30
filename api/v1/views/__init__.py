95d1d765f114d8bd3aab3e3409e5bd363cbf11a4
fatma tnani <fatmatnani08@gmail.com>
2023-07-30 16:13:51 -0700

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,3 +10,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Wildcard import of everything in the package api.v1.views.index
 # PEP8 will complain about it, but don’t worry, it’s normal and this file won’t be checked.
 from api.v1.views.index import *
+from api.v1.views.states import *
