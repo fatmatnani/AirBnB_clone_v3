7de65e46834ef9a4594c523e73a7f3e54261912e
fatma tnani <fatmatnani08@gmail.com>
2023-07-30 15:43:25 -0700

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 This module contains the principal application
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -21,6 +21,10 @@ def close_storage(exception):
     """Closes the database storage"""
     storage.close()
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handle 404 errors"""
+    return jsonify({"error": "Not found"}), 404
 
 # Inside if __name__ == "__main__":, run your Flask server (variable app)
 if __name__ == "__main__":
