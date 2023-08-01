a6d896bff20b2647d6d1519329bbba4c487099b2
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-08-01 01:23:09 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,20 +3,20 @@
 This module contains the principal application
 """
 
-from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 import os
+from flask_cors import CORS
+from flasgger import Swagger
 
-# Create a variable app, an instance of Flask
 app = Flask(__name__)
-
-# Register the blueprint app_views to your Flask instance app
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 # Declare a method to handle app.teardown_appcontext that calls storage.close()
-
 @app.teardown_appcontext
 def close_storage(exception):
     """Closes the database storage"""
@@ -26,7 +26,16 @@ def close_storage(exception):
 @app.errorhandler(404)
 def not_found(error):
     """Handle 404 errors"""
-    return (jsonify({"error": "Not found"}), 404)
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was created for the hbnb restful \
+    api project, all the documentation will be shown below',
+    'uiversion': 3}
+
+Swagger(app)
 
 
 # Inside if __name__ == "__main__":, run your Flask server (variable app)
