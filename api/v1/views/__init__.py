13330b240d2d273ecc399fa663c2a713a50c5912
Chukwuemeka Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-08-01 01:27:53 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -8,10 +8,13 @@ from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 # Wildcard import of everything in the package api.v1.views.index
-# PEP8 will complain about it, but don’t worry, it’s normal and this file won’t be checked.
+# PEP8 will complain about it, but don’t worry, it’s
+# normal and this file won’t be checked.
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import 
