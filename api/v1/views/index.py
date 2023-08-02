f9e7c51efbb70eb5c6db9c4a158927e58d0e2ac8
fatma tnani <fatmatnani08@gmail.com>
2023-08-01 22:59:31 -0700

    index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,3 +10,16 @@ from api.v1.views import app_views
 def status():
     """Return server status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Retrieves the number of each objects by type"""
+    return jsonify({
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
+    })
