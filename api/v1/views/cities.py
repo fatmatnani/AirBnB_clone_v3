ed4963a2c13e3fdf87ec51ff207dc2b192fd2add
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-08-03 10:51:42 -0700

    update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+This file contains the City module
+"""
+
 from flask import Flask, request, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
