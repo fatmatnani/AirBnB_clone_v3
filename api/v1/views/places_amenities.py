095d573bfea3cbe3a6ca90810b561253844bf393
fatma tnani <fatmatnani08@gmail.com>
2023-08-03 14:59:03 -0700

    places

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,65 @@
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.place import Place
+from models.amenity import Amenity
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+def get_amenities(place_id=None):
+    """Get all amenity objects for a specific place."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenities = [amenity.to_dict() for amenity in place.amenities]
+    return jsonify(amenities), 200
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['POST'], strict_slashes=False)
+def create_amenity(place_id=None):
+    """Create an amenity object for a specific place."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    if 'name' not in request.get_json():
+        return jsonify({'error': 'Missing name'}), 400
+    data = request.get_json()
+    data['place_id'] = place_id
+    amenity = Amenity(**data)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(place_id=None, amenity_id=None):
+    """Update an amenity object for a specific place."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    if not request.get_json():
+        return jsonify({'error': 'Not a JSON'}), 400
+    for key, value in request.get_json().items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(amenity, key, value)
+    amenity.save()
+    return jsonify(amenity.to_dict()), 200
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(place_id=None, amenity_id=None):
+    """Delete an amenity object for a specific place."""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    amenity.delete()
+    storage.save()
+    return jsonify({}), 200
+
